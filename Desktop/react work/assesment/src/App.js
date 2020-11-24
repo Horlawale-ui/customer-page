1c1345b5c0214f896d15342ab1b35a4a43d33508
Horlawale-ui <53876892+Horlawale-ui@users.noreply.github.com>
2020-11-24 08:14:43 +0100

    just UI

---
+++ Desktop/react work/assesment/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Profile from './profile.jsx';
+
+function App() {
+   
+    return(
+        <div>
+           <Profile/>
+        </div>
+    )
+}
+
+export default App;
---
+++ Desktop/react work/assesment/src/profile.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+const Profile =() => {
+    return(
+        <div>
+        <div className="container">
+              <div>
+                   <img style={{width:"260px", height:"260px",borderRadius:"90px"}}
+                    src="https://images.unsplash.com/photo-1549548731-a47a58b650f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfDJ8MHw%3D&auto=format&fit=crop&w=500&q=60" 
+                    alt="profile-image"
+                    />
+                </div>
+                <div>
+                    <h1>Aremu Khindave</h1>
+                    <div style={{display:"flex", justifyContent: "space-between", width:"105%"}}>
+                        <h3>20 Saved</h3>
+                        <h3>20 liked</h3>
+                        <h3>10 Favorite</h3>
+                    </div>
+                </div>
+        </div>
+            <div className="gallery">
+                <img className="items" src="https://images.unsplash.com/photo-1549548731-a47a58b650f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfDJ8MHw%3D&auto=format&fit=crop&w=500&q=60" 
+                alt="cart-image"/>
+                <img className="items" src="https://images.unsplash.com/photo-1549548731-a47a58b650f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfDJ8MHw%3D&auto=format&fit=crop&w=500&q=60" 
+                alt="cart-image"/>
+                <img className="items" src="https://images.unsplash.com/photo-1549548731-a47a58b650f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfDJ8MHw%3D&auto=format&fit=crop&w=500&q=60" 
+                alt="cart-image"/>
+                <img className="items" src="https://images.unsplash.com/photo-1549548731-a47a58b650f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfDJ8MHw%3D&auto=format&fit=crop&w=500&q=60" 
+                alt="cart-image"/>
+                <img className="items" src="https://images.unsplash.com/photo-1549548731-a47a58b650f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfDJ8MHw%3D&auto=format&fit=crop&w=500&q=60" 
+                alt="cart-image"/>
+            </div>
+        </div>
+    )
+
+}
+export default Profile;
+
